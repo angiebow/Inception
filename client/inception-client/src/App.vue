69e6ddab31cb533d3555c3cc0640fd38f8fa715f
Bimantara Putra Ernandra <bima.pe02@gmail.com>
2024-11-11 13:29:47 +0700

    home button

--- client/inception-client/src/App.vue
+++ client/inception-client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="bg-space min-h-screen text-white font-sans">
     <nav class="flex justify-center space-x-4 p-4 text-nebula">
+      <router-link to="/" class="hover:text-star">Home</router-link>
       <router-link to="/login" class="hover:text-star">Login</router-link>
       <router-link to="/register" class="hover:text-star">Register</router-link>
       <router-link to="/dashboard" class="hover:text-star">Dashboard</router-link>
